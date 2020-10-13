12296a5ec5d3e28cf082c82d0392f54920c79c94
repl.it user <replituser@example.com>
2020-10-13 08:11:02 +0000

    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,8 @@
+print("Program Started!")
+print("Please enter a standard character: ")
+char1 = input()
+if len(char1) == 1:
+  ascii = ord(char1)
+  print("The ASCII code for {} is {}.".format(char1, ascii))
+else:
+  print("input is not a single charater!")
