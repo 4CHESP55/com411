fca6c87afa774d32c4fbd6cff8e626f98909285a
repl.it user <replituser@example.com>
2020-11-03 09:29:52 +0000

    Added code to demonstrate a cwd.

---
+++ data/files/cwd.py
@@ -0,0 +1,13 @@
+def cwd():
+  import os
+  path = os.getcwd()
+  print(f"The current working folder is {path}")
+  print("The folder contains the following:")
+  for file in os.listdir(path):
+    print(file)
+
+def run():
+  print("Processing...")
+  cwd()
+
+run()
