43f7ddf286bb2d06ad87d407df9724b8e1967ffe
repl.it user <replituser@example.com>
2020-12-01 11:43:00 +0000

    Updated planet class to use a dictionary.

--- oop/planet.py
+++ oop/planet.py
@@ -1,21 +1,17 @@
 import human as human
 
 class Planet:
-  humans = []
-  robots = []
+  inhabitants = {
+  'humans': [],
+  'robots': []
+  }
 
   def __init__(self):
-    self.humans = Planet.humans
-    self.robots = Planet.robots
+    self.humans = Planet.inhabitants['humans']
+    self.robots = Planet.inhabitants['robots']
 
-  def add_human(self, name, age, move, grow, eat):
+  def add_human(self, name, age):
     newHuman = human.Human(name, age)
-    if move > 0:
-      newHuman.move(move)
-    if grow > 0:
-      newHuman.grow(grow)
-    if eat > 0:
-      newHuman.eat(eat)
     self.humans.append(newHuman)
   def add_robot(self, name, age):
     newRobot = human.Robot(name, age)
@@ -34,9 +30,10 @@ class Planet:
 
 planet = Planet()
 print(planet.__repr__())
-planet.add_human("Paul", 23, 0, 0, 0)
-planet.add_human("Paul", 23, 50, 3, 23)
+planet.add_human("Paul", 23)
+planet.add_human("Paul", 26)
 planet.add_robot("BEEP", 2)
 print(planet.__repr__())
 planet.remove_human(0)
 print(planet.__repr__())
+print(Planet.inhabitants)
